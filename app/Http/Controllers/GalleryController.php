8d4469953544ac2baf56b0c29771e113412a631c
Sabrina Markon <phpsitescripts@outlook.com>
2018-06-15 03:26:06 -0600

    make uploadPhotos method in GalleryController

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -9,5 +9,37 @@ class GalleryController extends Controller
     public function index() {
         return view('app');
     }
+
+    public function getPhotos() {
+
+    }
+
+    public function uploadPhotos(Request $request) {
+        $file = $request->file('file');
+        $ext = $file->extension();
+        $name = str_random(20) . '.' . $ext;
+        list($width, $height)= getimagesize($file);
+        $path = Storage::disk('public')->putFileAs(
+            'uploads', $file, $name
+        );
+        if ($path) {
+            $create = Auth::user()->photos()->create([
+                'uri' => $path,
+                'public' => false,
+                'height' => $height,
+                'width' => $width
+            ]);
+            if ($create) {
+                return response()->json([
+                    'uploaded' => true
+                ]);
+            }
+        }
+    }
+
+    public function deletePhotos(Request $request) {
+
+    }
+
 }
 
